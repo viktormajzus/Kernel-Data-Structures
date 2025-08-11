a2a4d3fbed3aad483ac692a8ff27aa7500b177c5
viktormajzus <victor.mayzus@gmail.com>
2025-08-11 11:46:41 +0300

    created a skeleton for the vector class

--- Kernel Data Structures/kds_include/vector.h
+++ Kernel Data Structures/kds_include/vector.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "alloc.h"
 
 namespace kds
 {
@@ -6,21 +7,49 @@ namespace kds
   class vector
   {
   private:
-    size_t m_size;
-    T* m_arrPtr;
-    size_t m_reservedSize;
+    size_t m_size{ 0 };
+    T* m_arrPtr{ nullptr };
+    size_t m_capacity{ 0 };
+    kds::allocator<T> m_alloc;
 
   public:
-    Vector();
-    explicit Vector(size_t size);
-    ~Vector();
+    explicit vector(ULONG tag = 'KDLS', ULONG flags = POOL_FLAG_NON_PAGED) noexcept;
+    explicit vector(size_t size, ULONG tag = 'KDLS', ULONG flags = POOL_FLAG_NON_PAGED) noexcept;
+    ~vector();
+
+    // Copy & move
+    vector(const vector& other) = delete;
+    vector& operator=(const vector& other) = delete;
+    vector(vector&& other) noexcept = delete;
+    vector& operator=(const vector&& other) noexcept = delete;
 
   public:
-    void reserve(size_t size);
-    T* at(size_t index);
-    const T* at(size_t index) const;
-    void clear();
-    bool empty() const;
+    // 
+    T* data() noexcept;
+    const T* data() const noexcept;
+    size_t size() const noexcept;
+    size_t capacity() const noexcept;
+    bool empty() const noexcept;
+
+    // Access functions
+    T* at(size_t index) noexcept;
+    const T* at(size_t index) const noexcept;
+    T* front() noexcept;
+    const T* front() const noexcept;
+    T* back() noexcept;
+    const T* back() const noexcept;
+
+    /// Unsafe, no bound checks
+    T& operator[](size_t index) noexcept;
+
+    // Operation functions
+    bool push_back(const T& value) noexcept;
+    void pop_back() noexcept;
+
+    // Memory functions
+    void clear() noexcept;
+    bool reserve(size_t n) noexcept;
+    bool shrink_to_fit() noexcept;
   };
 
 
@@ -29,8 +58,5 @@ namespace kds
   // Implementations
   // -----------------------------
 
-  template<typename T>
-  inline void vector<T>::reserve(const size_t size)
-  {
-  }
+
 }
